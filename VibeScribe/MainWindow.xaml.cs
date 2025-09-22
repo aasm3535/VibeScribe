63d954a5cd857cb87515e0a850668389ab4ff9ce
aasm <xn0tdev@gmail.com>
2025-09-22 18:01:48 +0300

    fix: add using Microsoft.UI.Xaml.Shapes for Rectangle in MainWindow.xaml.cs

--- VibeScribe/MainWindow.xaml.cs
+++ VibeScribe/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI.Xaml.Shapes;
 using Microsoft.Windows.AppNotifications;
 using Microsoft.Windows.AppNotifications.Builder;
 using System;
